bc01c6c7800d68f4eced800d35464c5caf61acb8
D19C <cruzbedoya.dc@gmail.com>
2024-03-07 20:25:39 -0500

    fixes

---
+++ backend/mosaic/base/models/__init__.py
@@ -0,0 +1,6 @@
+from base.models.email_template import EmailTemplate
+from base.models.email_tracking import EmailTracking
+from base.models.environment_variables import EnvironmentVariables
+from base.models.creator import Creator
+from base.models.admin import Admin
+from base.models.user import User
